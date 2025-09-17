d2503ceea5474a37bb7f24a8f0fb5b829c698744
swood81 <swood81@gatech.edu>
2025-09-17 23:22:26 +0000

    Added a cache buster

--- app/main.py
+++ app/main.py
@@ -2,14 +2,24 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+import pathlib
 
 app = FastAPI(debug=True)
+
+CSS_PATH = pathlib.Path("app/static/css/output.css")
+
+def css_build_id():
+    try:
+        return str(int(CSS_PATH.stat().st_mtime))
+    except FileNotFoundError:
+        return "dev"
 
 # Mount the static directory for CSS/JS files
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
 # Set up Jinja2 templates
 templates = Jinja2Templates(directory="app/templates")
+templates.env.globals["build_id"] = css_build_id()
 
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
